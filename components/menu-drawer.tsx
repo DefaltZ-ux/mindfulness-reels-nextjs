7363033cad2dbe32e7a29f19740fc7eada08d2fd
arkediagithub <bd@arkedia.in>
2025-09-17 21:42:57 +0530

    drawer touch problem

--- components/menu-drawer.tsx
+++ components/menu-drawer.tsx
@@ -9,6 +9,7 @@ import {
   DrawerDescription,
   DrawerFooter,
   DrawerHeader,
+  DrawerOverlay,
   DrawerTitle,
   DrawerTrigger,
 } from "./ui/drawer";
@@ -17,26 +18,44 @@ import { EllipsisVertical } from "lucide-react";
 export default function MenuDrawer() {
   // const isMobile = useIsMobile();
 
+  function handleTouch(e: React.TouchEvent<HTMLDivElement>) {
+    e.preventDefault();
+    e.stopPropagation();
+  }
+
   return (
     <>
       <Drawer>
-        <DrawerTrigger >
+        <DrawerTrigger>
           <EllipsisVertical className="text-white -mb-1" />
         </DrawerTrigger>
-        <DrawerContent className="max-w-3xl mx-auto">
-          <DrawerHeader>
-            <DrawerTitle>Mindfulness</DrawerTitle>
-            <DrawerDescription>Find your inner peace</DrawerDescription>
-          </DrawerHeader>
-          <div className="text-justify">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo maxime deleniti consequatur, iste ut ipsum dicta, sequi illum aliquam quaerat mollitia reprehenderit aliquid est repellendus nihil ipsa alias consectetur quisquam possimus? Sunt animi vero dicta culpa officia tempora adipisci eaque laboriosam deserunt, fuga sapiente? Aut, quam necessitatibus praesentium repellendus recusandae explicabo soluta delectus dicta deserunt, fugit error animi, hic magni! Debitis placeat vero odit molestias dolor magni, sint, minima quas, laboriosam ut nulla iure quis. Atque, alias eaque inventore id quas odit vero sunt omnis maiores voluptatem officia consequatur exercitationem quaerat velit doloremque quos illo tenetur fuga dignissimos reprehenderit necessitatibus.
-          </div>
-          <DrawerFooter>
-            <DrawerClose>
-              <Button variant="outline">Close</Button>
-            </DrawerClose>
-          </DrawerFooter>
-        </DrawerContent>
+        <DrawerOverlay onTouchStart={handleTouch}>
+          <DrawerContent className="max-w-3xl mx-auto">
+            <DrawerHeader>
+              <DrawerTitle>Mindfulness</DrawerTitle>
+              <DrawerDescription>Find your inner peace</DrawerDescription>
+            </DrawerHeader>
+            <div className="text-justify">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo
+              maxime deleniti consequatur, iste ut ipsum dicta, sequi illum
+              aliquam quaerat mollitia reprehenderit aliquid est repellendus
+              nihil ipsa alias consectetur quisquam possimus? Sunt animi vero
+              dicta culpa officia tempora adipisci eaque laboriosam deserunt,
+              fuga sapiente? Aut, quam necessitatibus praesentium repellendus
+              recusandae explicabo soluta delectus dicta deserunt, fugit error
+              animi, hic magni! Debitis placeat vero odit molestias dolor magni,
+              sint, minima quas, laboriosam ut nulla iure quis. Atque, alias
+              eaque inventore id quas odit vero sunt omnis maiores voluptatem
+              officia consequatur exercitationem quaerat velit doloremque quos
+              illo tenetur fuga dignissimos reprehenderit necessitatibus.
+            </div>
+            <DrawerFooter>
+              <DrawerClose>
+                <Button variant="outline">Close</Button>
+              </DrawerClose>
+            </DrawerFooter>
+          </DrawerContent>
+        </DrawerOverlay>
       </Drawer>
     </>
   );
