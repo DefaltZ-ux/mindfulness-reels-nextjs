df298529416fc2d8f021e050d1818580a8108890
arkediagithub <bd@arkedia.in>
2025-09-20 18:05:23 +0530

    rename navigation to app navigation

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Suspense } from "react";
-import { Navigation } from "@/components/Navigation";
+import AppNavigation from "@/components/AppNavigation";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -33,7 +33,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <Suspense fallback={<div>Loading...</div>}>
-          <Navigation />
+          <AppNavigation />
           <main>{children}</main>
         </Suspense>
       </body>
